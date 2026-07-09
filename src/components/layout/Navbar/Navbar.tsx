cc17c64c64644cfee6e4707b6e33a9faf0c65f1a
macyman1 <malaviya244@gmail.com>
2026-07-09 13:51:57 +0530

    feat: add 'All' category link to navbar

--- src/components/layout/Navbar/Navbar.tsx
+++ src/components/layout/Navbar/Navbar.tsx
@@ -45,6 +45,7 @@ export function Navbar() {
 
   const navCategories = [
     { name: 'Home', path: '/', highlight: false },
+    { name: 'All', path: '/products', highlight: false },
     ...(freshPicks
       ? [{
           name: freshPicks.name,
